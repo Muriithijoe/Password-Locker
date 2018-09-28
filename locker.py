9613c09d429033c16b3ce9ab4f0f9bcb0b4cbcdf
Joe <joeace2000@gmail.comgit config --global user.name Muriithijoe>
2018-09-28 10:13:36 +0300

    added docstrings for user class

--- locker.py
+++ locker.py
@@ -2,3 +2,21 @@ class User:
     '''
     class for passing all instances of the user
     '''
+
+    def __init__(self,username,account_name,email,password):
+        '''
+        initiates properties of my objects
+
+        Args:
+            username:New username of the account
+            account_name:New account name
+            email:New email address
+            password:New password
+        '''
+        self.username = username
+        self.account_name = account_name
+        self.email = email
+        self.password = password
+
+        
+
